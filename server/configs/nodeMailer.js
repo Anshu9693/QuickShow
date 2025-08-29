0f2780f149f3c7649830801cb2d68d77e30257e2
Anshu9693 <anshukumarsah8585@gmail.com>
2025-08-29 16:48:48 +0530

    smtp fixed

--- server/configs/nodeMailer.js
+++ server/configs/nodeMailer.js
@@ -1,23 +1,29 @@
-// import nodemailer from 'nodemailer'
+import nodemailer from 'nodemailer'
 
-// const transporter = nodemailer.createTransport({
-//   host: "smtp-relay.brevo.com",
-//   port: 587,
-//   auth: {
-//     user: process.env.SMTP_USER,
-//     pass:process.env.SMTP_PASS,
-//   },
-// });
+const transporter = nodemailer.createTransport({
+  host: "smtp-relay.brevo.com",
+  port: 587,
+  secure: false, // must be false for port 587
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS,
+  },
+});
 
 
-// const sendEmail= async({to,subject,body})=>{
-//     const response = await transporter.sendMail({
-//         from:process.env.SENDER_EMAIL,
-//         to,
-//         subject,
-//         html:body
-//     })
-//     return response;
-// }
+const sendEmail = async ({ to, subject, body }) => {
+  try {
+    const response = await transporter.sendMail({
+      from: process.env.SENDER_EMAIL,
+      to,
+      subject,
+      html: body,
+    });
+    return response;
+  } catch (error) {
+    console.error("Email error:", error);
+    throw error;
+  }
+};
 
-// export default sendEmail;
+export default sendEmail;
--- server/inngest/index.js
+++ server/inngest/index.js
@@ -2,7 +2,7 @@ import { Inngest } from "inngest";
 import  User  from "../models/user.js";
 import Booking from "../models/Booking.js";
 import Show from "../models/Show.js";
-// import sendEmail from "../configs/nodeMailer.js";
+import sendEmail from "../configs/nodeMailer.js";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({ id: "Movie-Ticket-Booking" });
@@ -82,42 +82,42 @@ const releaseSeatsAndDeleteBooking = inngest.createFunction(
   }
 )
 
-// const sendBookingConformationEmail = inngest.createFunction(
-//   {id:"send-Booking-Conformation-Email"},
-// {event:"app/show.booked"},
-// async({event,step})=>{
-// const {bookingId}=event.data;
-// const booking = await Booking.findById(bookingId).populate({
-//   path:'show',
-//   populate:{
-//     path:"movie",model:"Movie"
-//   }
-// }).populate("user");
-//         await sendEmail({
-//           to:booking.user.email,
-//           subject:`Payment Conformation "${booking.show.movie.title}"Booked!`,
-//           body:
-//           `<div style="font-family: Arial, sans-serif; line-height: 1.5;">
-//   <h2>Hi ${booking.user.name},</h2>
-//   <p>
-//     Your booking for 
-//     <strong style="color: #F84565;">${booking.show.movie.title}</strong> 
-//     is confirmed.
-//   </p>
+const sendBookingConformationEmail = inngest.createFunction(
+  {id:"send-Booking-Conformation-Email"},
+{event:"app/show.booked"},
+async({event,step})=>{
+const {bookingId}=event.data;
+const booking = await Booking.findById(bookingId).populate({
+  path:'show',
+  populate:{
+    path:"movie",model:"Movie"
+  }
+}).populate("user");
+        await sendEmail({
+          to:booking.user.email,
+          subject:`Payment Conformation "${booking.show.movie.title}"Booked!`,
+          body:
+          `<div style="font-family: Arial, sans-serif; line-height: 1.5;">
+  <h2>Hi ${booking.user.name},</h2>
+  <p>
+    Your booking for 
+    <strong style="color: #F84565;">${booking.show.movie.title}</strong> 
+    is confirmed.
+  </p>
 
-//   <p>
-//     <strong>Date:</strong> 
-//     ${new Date(booking.show.showDateTime).toLocaleDateString('en-US', { timeZone: 'Asia/Kolkata' })}<br/>
-//     <strong>Time:</strong> 
-//     ${new Date(booking.show.showDateTime).toLocaleTimeString('en-US', { timeZone: 'Asia/Kolkata' })}
-//   </p>
+  <p>
+    <strong>Date:</strong> 
+    ${new Date(booking.show.showDateTime).toLocaleDateString('en-US', { timeZone: 'Asia/Kolkata' })}<br/>
+    <strong>Time:</strong> 
+    ${new Date(booking.show.showDateTime).toLocaleTimeString('en-US', { timeZone: 'Asia/Kolkata' })}
+  </p>
 
-//   <p>Enjoy the show! 🍿</p>
-//   <p>Thanks for booking with us!<br/>— QuickShow Team</p>
-// </div>
-// `
-//         })
-// })
+  <p>Enjoy the show! 🍿</p>
+  <p>Thanks for booking with us!<br/>— QuickShow Team</p>
+</div>
+`
+        })
+})
 
 // Create an empty array where we'll export future Inngest functions
 export const functions = [syncUserCreation, syncUserDeletion,syncUserUpdation,releaseSeatsAndDeleteBooking,
